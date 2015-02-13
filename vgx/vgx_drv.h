0be46be4c3a12c6374b6c5b890ca5d0d968575d4
Marco Paland <info@paland.com>
2015-02-13 18:49:52 +0100

    New color value handling

--- vgx/vgx_drv.h
+++ vgx/vgx_drv.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 // \author (c) Marco Paland (info@paland.com)
-//             2014-2014, PALANDesign Hannover, Germany
+//             2014-2015, PALANDesign Hannover, Germany
 //
 // \license The MIT License (MIT)
 //
@@ -35,82 +35,45 @@
 
 namespace vgx {
 
-/**
- * Display hardware interface
- */
-typedef enum enum_interface_type
-{
-  interface_mem = 0,    // memory mapped interface
-  interface_spi,        // SPI interface
-  interface_i2c,        // I²C interface
-  interface_can,        // CAN interface
-  interface_usb,        // USB interface
-  interface_uart,       // UART interface
-  intercace_cust        // custom interface (bit banging etc.)
-} interface_type;
-
-/**
- * Display orientation, the driver takes care of screen rotation
- * (0,0) is always logical (left/top)
- */
-typedef enum enum_orientation_type
-{
-  orientation_0 = 0,
-  orientation_90,
-  orientation_180,
-  orientation_270
-} orientation_type;
-
-
-/**
- * supported color formats
-  bits depth  format    usage
-  ##########################################################################
-   1     1           1   monochrome
-   2     2           2   2 bit grayscale
-   4     4           4   4 bit grayscale
-   8     8           8   8 bit grayscale
-   8     8        CLUT   8 bit color lookup table for 256 defined colors
-   8     8       3-3-2    256 RGB display - no palette
-  15    15       5-5-5    32k RGB display
-  16    16       5-6-5    64k RGB display
-  16    12     A-4-4-4     4k RGB display with 4 bit alpha blending
-  16    15     A-5-5-5    32k RGB display with 1 bit alpha blending
-  18    18     0-6-6-6   262k RGB display
-  24    24     0-8-8-8    16M RGB display
-  32    24     A-8-8-8    16M RGB display with alpha blending
-                 R-G-B
-*/
-typedef enum enum_color_format_type
-{
-  color_format_L1 = 0,    // 1 bit luminance (monochrome)
-  color_format_L2,        // 2 bit
-  color_format_L4,        // 4 bit
-  color_format_L8,        // 8 bit, 256 luminance (gray) levels
-  color_format_C8,        // 8 bit CLUT (color lookup table) - reserved for future, unsupported yet
-  color_format_RGB332,
-  color_format_RGB555,
-  color_format_RGB565,
-  color_format_RGB666,
-  color_format_RGB888,
-  color_format_ARGB1555,
-  color_format_ARGB4444,
-  color_format_ARGB8888
-} color_format_type;
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-
 class drv : public gpr
 {
 public:
+
+  /**
+   * Display hardware interface
+   */
+  typedef enum enum_interface_type
+  {
+    interface_mem = 0,    // memory mapped interface
+    interface_dio,        // digital IO interface
+    interface_spi,        // SPI interface
+    interface_i2c,        // I²C interface
+    interface_can,        // CAN interface
+    interface_usb,        // USB interface
+    interface_uart,       // UART interface
+    intercace_cust        // custom interface (bit banging etc.)
+  } interface_type;
+
+  /**
+   * Display orientation, the driver takes care of screen rotation
+   * (0,0) is always logical (left/top)
+   */
+  typedef enum enum_orientation_type
+  {
+    orientation_0 = 0,
+    orientation_90,
+    orientation_180,
+    orientation_270
+  } orientation_type;
+
+  ///////////////////////////////////////////////////////////////////////////////
+
   /**
    * ctor
    * \param xsize Screen width
    * \param ysize Screen height
-   * \param xoffset X offset within the screen, relative to top/left corner, usefull if border pixels are behind a bezel
-   * \param yoffset Y offset within the screen, relative to top/left corner, usefull if border pixels are behind a bezel
+   * \param xoffset X offset within the screen, relative to top/left corner, useful if border pixels are behind a bezel
+   * \param yoffset Y offset within the screen, relative to top/left corner, useful if border pixels are behind a bezel
    */
   drv(std::uint16_t xsize, std::uint16_t ysize, std::int16_t xoffset, std::int16_t yoffset)
     : xsize_(xsize)
@@ -152,17 +115,18 @@ public:
 
 
   /////////////////////////////////////////////////////////////////////////////
+  // G R A P H I C   D I S P L A Y S
+  //
   // The functions in this section are MANDATORY FOR GRAPHIC drivers!
-  // Every graphic display driver MUST implement them., even if unsupported
-  // In case of alpha numeric display, leave unimplemented in derived class
+  // Every graphic display driver MUST implement/override them.
+  // In case of alpha numeric display, leave unimplemented
 
   /**
    * Set pixel (in actual drawing color)
    * \param x X value
    * \param y Y value
    */
-  virtual void pixel_set(std::int16_t x, std::int16_t y)
-  { (void)x; (void)y; }
+  //virtual void drv_pixel_set(std::int16_t x, std::int16_t y);
 
   /**
    * Set pixel in given color, the color doesn't change the actual drawing color
@@ -170,8 +134,7 @@ public:
    * \param y Y value
    * \param color Color of pixel in ARGB format
    */
-  virtual void pixel_set_color(std::int16_t x, std::int16_t y, std::uint32_t color)
-  { (void)x; (void)y; (void)color; }
+  //virtual void drv_pixel_set_color(std::int16_t x, std::int16_t y, color::value_type color);
 
   /**
    * Get pixel color
@@ -179,10 +142,48 @@ public:
    * \param y Y value
    * \return Color of pixel in ARGB format
    */
-  virtual std::uint32_t pixel_get(std::int16_t x, std::int16_t y) const
-  { (void)x; (void)y; return 0U; }
+  //virtual color::value_type drv_pixel_get(std::int16_t x, std::int16_t y) const;
+
 
   /////////////////////////////////////////////////////////////////////////////
+  // A L P H A   N U M E R I C   D I S P L A Y S
+  //
+  // The functions in this section are MANDATORY FOR ALPHA NUMERIC drivers!
+  // Every alpha numeric display driver MUST implement/override them.
+  // In case of a graphic display, leave unimplemented
+
+  /**
+  * Select the font
+  * \param Reference to font to use
+  * \return true if font set successfully
+  */
+  //virtual bool drv_text_set_font(const font_type& font);
+
+  /**
+  * Set the new text position
+  * \param x X value in pixel on graphic displays, char pos on alpha displays
+  * \param y Y value in pixel on graphic displays, char pos on alpha displays
+  * \return true if position is set successfully
+  */
+  //virtual bool drv_text_set_pos(std::int16_t x, std::int16_t y);
+
+  /**
+  * Set the text mode
+  * \param mode Set normal or inverse video
+  * \return true if successful
+  */
+  //virtual bool drv_text_set_mode(text_mode_type mode);
+
+  /**
+  * Output one character
+  * \param ch Output character in ASCII/UNICODE (NOT UTF-8) format
+  */
+  //virtual void drv_text_char(std::uint16_t ch);
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  // B A S E  F U N C T I O N S
+  //
 
   /**
    * Returns the screen width
@@ -220,67 +221,67 @@ protected:
    * \param color Internal 32 bpp ARGB color value
    * \return Native head color value
    */
-  inline std::uint8_t color_to_head_L1(std::uint32_t color) const
+  inline std::uint8_t color_to_head_L1(color::value_type color) const
   { return static_cast<std::uint8_t>((color & 0x00FFFFFFUL) != (std::uint32_t)0U ? 1U : 0U); }
 
-  inline std::uint8_t color_to_head_L2(std::uint32_t color) const
-  { return static_cast<std::uint8_t>(((std::uint16_t)((std::uint16_t)color_get_red(color) + (std::uint16_t)color_get_green(color) + (std::uint16_t)color_get_blue(color)) / 3U) >> 6U); }
+  inline std::uint8_t color_to_head_L2(color::value_type color) const
+  { return static_cast<std::uint8_t>(((std::uint16_t)((std::uint16_t)color::get_red(color) + (std::uint16_t)color::get_green(color) + (std::uint16_t)color::get_blue(color)) / 3U) >> 6U); }
 
-  inline std::uint8_t color_to_head_L4(std::uint32_t color) const
-  { return static_cast<std::uint8_t>(((std::uint16_t)((std::uint16_t)color_get_red(color) + (std::uint16_t)color_get_green(color) + (std::uint16_t)color_get_blue(color)) / 3U) >> 4U); }
+  inline std::uint8_t color_to_head_L4(color::value_type color) const
+  { return static_cast<std::uint8_t>(((std::uint16_t)((std::uint16_t)color::get_red(color) + (std::uint16_t)color::get_green(color) + (std::uint16_t)color::get_blue(color)) / 3U) >> 4U); }
 
-  inline std::uint8_t color_to_head_L8(std::uint32_t color) const
-  { return static_cast<std::uint8_t>((std::uint16_t)((std::uint16_t)color_get_red(color) + (std::uint16_t)color_get_green(color) + (std::uint16_t)color_get_blue(color)) / 3U); }
+  inline std::uint8_t color_to_head_L8(color::value_type color) const
+  { return static_cast<std::uint8_t>((std::uint16_t)((std::uint16_t)color::get_red(color) + (std::uint16_t)color::get_green(color) + (std::uint16_t)color::get_blue(color)) / 3U); }
 
-  inline std::uint8_t color_to_head_RGB332(std::uint32_t color) const
-  { return static_cast<std::uint8_t>((std::uint8_t)(color_get_red(color) & 0xE0U) | (std::uint8_t)((color_get_green(color) & 0xE0U) >> 3U) | (std::uint8_t)((color_get_blue(color)) >> 6U)); }
+  inline std::uint8_t color_to_head_RGB332(color::value_type color) const
+  { return static_cast<std::uint8_t>((std::uint8_t)(color::get_red(color) & 0xE0U) | (std::uint8_t)((color::get_green(color) & 0xE0U) >> 3U) | (std::uint8_t)((color::get_blue(color)) >> 6U)); }
 
-  inline std::uint16_t color_to_head_RGB555(std::uint32_t color) const
-  { return static_cast<std::uint16_t>(((std::uint16_t)(color_get_red(color) & 0xF8U) << 10U) | ((std::uint16_t)(color_get_green(color) & 0xF8U) << 5U) | (std::uint16_t)(color_get_blue(color) >> 3U)); }
+  inline std::uint16_t color_to_head_RGB555(color::value_type color) const
+  { return static_cast<std::uint16_t>(((std::uint16_t)(color::get_red(color) & 0xF8U) << 10U) | ((std::uint16_t)(color::get_green(color) & 0xF8U) << 5U) | (std::uint16_t)(color::get_blue(color) >> 3U)); }
 
-  inline std::uint16_t color_to_head_RGB565(std::uint32_t color) const
-  { return static_cast<std::uint16_t>(((std::uint16_t)(color_get_red(color) & 0xF8U) << 11U) | ((std::uint16_t)(color_get_green(color) & 0xFCU) << 5U) | (std::uint16_t)(color_get_blue(color) >> 3U)); }
+  inline std::uint16_t color_to_head_RGB565(color::value_type color) const
+  { return static_cast<std::uint16_t>(((std::uint16_t)(color::get_red(color) & 0xF8U) << 11U) | ((std::uint16_t)(color::get_green(color) & 0xFCU) << 5U) | (std::uint16_t)(color::get_blue(color) >> 3U)); }
 
-  inline std::uint32_t color_to_head_RGB666(std::uint32_t color) const
-  { return static_cast<std::uint32_t>(((std::uint32_t)(color_get_red(color) & 0xFCU) << 12U) | ((std::uint32_t)(color_get_green(color) & 0xFCU) << 6U) | (std::uint32_t)(color_get_blue(color) >> 2U)); }
+  inline std::uint32_t color_to_head_RGB666(color::value_type color) const
+  { return static_cast<std::uint32_t>(((std::uint32_t)(color::get_red(color) & 0xFCU) << 12U) | ((std::uint32_t)(color::get_green(color) & 0xFCU) << 6U) | (std::uint32_t)(color::get_blue(color) >> 2U)); }
 
-  inline std::uint32_t color_to_head_RGB888(std::uint32_t color) const
+  inline std::uint32_t color_to_head_RGB888(color::value_type color) const
   { return static_cast<std::uint32_t>(color & 0x00FFFFFFUL); }
 
-  inline std::uint32_t color_from_head_L1(std::uint8_t head_color) const
-  { return head_color ? VGX_COLOR_WHITE : VGX_COLOR_BLACK; }
+  inline color::value_type color_from_head_L1(std::uint8_t head_color) const
+  { return head_color ? color::white : color::black; }
 
-  inline std::uint32_t color_from_head_L2(std::uint8_t head_color) const
-  { return color_dim(VGX_COLOR_WHITE, (255U / 3U * (head_color & 0x03U))); }
+  inline color::value_type color_from_head_L2(std::uint8_t head_color) const
+  { return color::dim(color::white, (255U / 3U * (head_color & 0x03U))); }
 
-  inline std::uint32_t color_from_head_L4(std::uint8_t head_color) const
-  { return color_dim(VGX_COLOR_WHITE, (255U / 15U * (head_color & 0x0FU))); }
+  inline color::value_type color_from_head_L4(std::uint8_t head_color) const
+  { return color::dim(color::white, (255U / 15U * (head_color & 0x0FU))); }
 
-  inline std::uint32_t color_from_head_L8(std::uint8_t head_color) const
-  { return color_dim(VGX_COLOR_WHITE, head_color); }
+  inline color::value_type color_from_head_L8(std::uint8_t head_color) const
+  { return color::dim(color::white, head_color); }
 
-  inline std::uint32_t color_from_head_RGB332(std::uint8_t head_color) const
-  { return color_rgb(static_cast<std::uint8_t>(head_color & 0xE0U), static_cast<std::uint8_t>((head_color & 0x1CU) << 3U), static_cast<std::uint8_t>((head_color & 0x03U) << 6U)); }
+  inline color::value_type color_from_head_RGB332(std::uint8_t head_color) const
+  { return color::rgb(static_cast<std::uint8_t>(head_color & 0xE0U), static_cast<std::uint8_t>((head_color & 0x1CU) << 3U), static_cast<std::uint8_t>((head_color & 0x03U) << 6U)); }
 
-  inline std::uint32_t color_from_head_RGB555(std::uint16_t head_color) const
-  { return color_rgb(static_cast<std::uint8_t>((head_color & 0x7C00U) >> 7U), static_cast<std::uint8_t>((head_color & 0x03E0U) >> 2U), static_cast<std::uint8_t>((head_color & 0x001FU) << 3U)); }
+  inline color::value_type color_from_head_RGB555(std::uint16_t head_color) const
+  { return color::rgb(static_cast<std::uint8_t>((head_color & 0x7C00U) >> 7U), static_cast<std::uint8_t>((head_color & 0x03E0U) >> 2U), static_cast<std::uint8_t>((head_color & 0x001FU) << 3U)); }
 
-  inline std::uint32_t color_from_head_RGB565(std::uint16_t head_color) const
-  { return color_rgb(static_cast<std::uint8_t>((head_color & 0xF800U) >> 8U), static_cast<std::uint8_t>((head_color & 0x07E0U) >> 3U), static_cast<std::uint8_t>((head_color & 0x001FU) << 3U)); }
+  inline color::value_type color_from_head_RGB565(std::uint16_t head_color) const
+  { return color::rgb(static_cast<std::uint8_t>((head_color & 0xF800U) >> 8U), static_cast<std::uint8_t>((head_color & 0x07E0U) >> 3U), static_cast<std::uint8_t>((head_color & 0x001FU) << 3U)); }
 
-  inline std::uint32_t color_from_head_RGB666(std::uint32_t head_color) const
-  { return color_rgb(static_cast<std::uint8_t>((head_color & 0x0003F000UL) >> 10U), static_cast<std::uint8_t>((head_color & 0x00000FC0UL) >> 4U), static_cast<std::uint8_t>((head_color & 0x0000003FUL) << 2U)); }
+  inline color::value_type color_from_head_RGB666(std::uint32_t head_color) const
+  { return color::rgb(static_cast<std::uint8_t>((head_color & 0x0003F000UL) >> 10U), static_cast<std::uint8_t>((head_color & 0x00000FC0UL) >> 4U), static_cast<std::uint8_t>((head_color & 0x0000003FUL) << 2U)); }
 
-  inline std::uint32_t color_from_head_RGB888(std::uint32_t head_color) const
-  { return static_cast<std::uint32_t>(head_color & 0x00FFFFFFUL); }
+  inline color::value_type color_from_head_RGB888(std::uint32_t head_color) const
+  { return static_cast<color::value_type>(head_color & 0x00FFFFFFUL); }
 
 
   ///////////////////////////////////////////////////////////////////////////////
   // H A R D W A R E   A C C E S S
   //
-  // Define the according functions in your project
+  // Define all IO functions in your project
   //
-  struct out {
+  struct io {
     // Direct memory access, write
     template<typename VALUE_TYPE>
     static void mem_set(volatile void* address, VALUE_TYPE value);
@@ -293,30 +294,32 @@ protected:
     template<typename VALUE_TYPE>
     static VALUE_TYPE mem_get(volatile void* address);
 
-    // SPI access
-    static bool spi(std::uint16_t device_id,
-                    const std::uint8_t* data_out, std::uint16_t data_out_length,
-                    std::uint8_t* data_in, std::uint16_t data_in_length);
+    /**
+     * IO write/read access to device
+     * The bytes read are only valid on interfaces, where writing and reading is simultaneous, like SPI
+     * \param if_type Interface type
+     * \param device_id Logical device ID of the according interface
+     * \param data_out Data transmit buffer
+     * \param data_out_length Data length to send
+     * \param data_in Data receive buffer
+     * \param data_in_length Additional input length
+     * \return true if successful
+     */ 
+    static bool dev_set(interface_type if_type, std::uint16_t device_id,
+                        const std::uint8_t* data_out, std::uint16_t data_out_length,
+                        std::uint8_t* data_in, std::uint16_t data_in_length);
 
-    // I²C access
-    static bool i2c(std::uint8_t device_id,
-                    const std::uint8_t* data_out, std::uint16_t data_out_length,
-                    std::uint8_t* data_in, std::uint16_t data_in_length);
-
-    // UART access, send
-    static bool uart_tx(std::uint8_t port,
-                        const std::uint8_t* data_out, std::uint16_t data_out_length);
-
-    // UART access, receive
-    static bool uart_rx(std::uint8_t port,
-                        std::uint8_t* data_in, std::uint16_t data_in_length, std::uint16_t& bytes_received);
-
-    // Digital I/O, pin set (used for bit banging, etc)
-    static bool dio_set(std::uint8_t id, bool value);
-
-    // Digital I/O, pin get
-    static bool dio_get(std::uint8_t id);
-  };
+    /**
+     * IO read access from device
+     * \param if_type Interface type
+     * \param device_id Logical device ID of the according interface
+     * \param data_in Data receive buffer
+     * \param data_in_length Maximum buffer size on input, received chars on output
+     * \return true if successful
+     */ 
+    static bool dev_get(interface_type if_type, std::uint16_t device_id,
+                        std::uint8_t* data_in, std::uint16_t* data_in_length);
+  } io_;
 
 protected:
   const std::uint16_t xsize_;     // x screen size in pixel (graphic) or chars (alpha)
